958cf45f980f2f549191603448dec5a8ec16c7de
ishabaral <baralisha.16@gmail.com>
2021-08-05 15:43:26 +0545

    jest with enzyme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is counter app</h1>
+      <button id="increment-btn" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <button id="decrement-btn" onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+      <div id="counter-value">{count}</div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// setup file
+import { shallow } from "enzyme";
+
+let wrapper;
+beforeEach(() => {
+  //before each function runs the test include this code
+  wrapper = shallow(<App />); //It only checks for the content of <App />. if there are other components in <App />, shallow won't go inside it
+}); //alternatively you can use mount && import it for enzyme
+
+describe("Counter testing", () => {
+  test("Render the counter heading", () => {
+    // console.log(wrapper.debug());
+    expect(wrapper.find("h1").text()).toContain("This is counter app");
+  });
+
+  test("render a button with a text of `increment`", () => {
+    expect(wrapper.find("#increment-btn").text()).toBe("Increment");
+  });
+
+  test("render the initial value of state in a div", () => {
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
+
+  test("render the click event of increment button and increment counter value", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+  });
+
+  test("render the click event of decrement button and decrement counter value", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+    wrapper.find("#decrement-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
 });
